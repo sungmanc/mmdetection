75f8e72b15582e0cb247af1a9c30f1893442065f
sungmanc <sungman.cho@intel.com>
2022-08-03 13:00:37 +0900

    add metanet and swint for upm

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -18,9 +18,10 @@ from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .trident_resnet import TridentResNet
+from .metanet import OTEMetaNet
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
     'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'Darknet',
-    'ResNeSt', 'TridentResNet', 'CSPDarknet'
+    'ResNeSt', 'TridentResNet', 'CSPDarknet', 'OTEMetaNet'
 ]
